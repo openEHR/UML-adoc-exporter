e77fb7a41f865c3a21520ed3ee52f19039534029
wolandscat <thomas.beale@openehr.org>
2025-03-11 14:55:14 -0600

    Support Link template passed as argument, instead of hard-wired Link construction.

--- UmlAdocExporter/src/org/openehr/adoc/magicdraw/UmlExporterDefinitions.java
+++ UmlAdocExporter/src/org/openehr/adoc/magicdraw/UmlExporterDefinitions.java
@@ -6,7 +6,7 @@ public abstract class UmlExporterDefinitions {
 
     public static final String BARE_QUOTE_REGEX = "[A-Za-z0-9_][A-Za-z0-9_. ]+";
 
-    public static final String SPEC_RELEASE_PATTERN_DEFAULT = "{%s_release}";
+    public static final String DEFAULT_SPEC_LINK_TEMPLATE ="/releases/${component_prefix}${component}/{${component_lower}_release}/${spec_name}.html";
 
     public static final String ROOT_PACKAGE_NAME_DEFAULT = "openehr";
 
